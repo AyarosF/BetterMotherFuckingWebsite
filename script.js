df820c5fd72e5f5a8e05a01c215076c2250e7ffa
AyarosF <sorayafructuoso@gmail.com>
2018-06-12 20:40:34 +0200

    Création du mode psyche

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-setTimeout(function() {
-  alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
-}, 10000);
+// setTimeout(function() {
+//   alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+// }, 10000);
+
+var colors = ["magenta", "white"],
+    index = 0;
+
+document.getElementById("psyche").onclick = function() {
+  document.body.style.background = colors[index];
+  index = (index + 1) % colors.length;
+}
