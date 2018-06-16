e4e5eb869d04f46c43dfb2e3c1392dbae13bb780
AyarosF <sorayafructuoso@gmail.com>
2018-06-16 23:52:29 +0200

    Ajout du retour à la normal après clic

--- script.js
+++ script.js
@@ -2,26 +2,45 @@ setTimeout(function() {
   alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
 }, 10000);
 
-// var colors = ["magenta", "white"],
-//     index = 0;
-//
-// document.getElementById("psyche").onclick = function() {
-//   document.body.style.background = colors[index];
-//   index = (index + 1) % colors.length;
-// } -> permet de faire revenir à la normal après 2 cliks (que background)
 
 
-function changeColorAndFont() {
-  document.body.style.background = 'magenta';
+var click = 0;
+
+function onClick() {
+
+if (click == 0) {
+
+  document.body.style.background = "magenta";
   var x = document.getElementsByClassName("greencolor");
   var i;
   for (i = 0; i < x.length; i++) {
-      x[i].style.color = "green";
-      x[i].style.fontFamily = "Comic Sans Ms";
-      }
+        x[i].style.color = "green";
+        x[i].style.fontFamily = "Comic Sans Ms";
+        }
   var y = document.getElementsByClassName("bluecolor");
   for (i = 0; i < y.length; i++) {
-      y[i].style.color = "blue";
-      y[i].style.fontFamily = "papyrus";
-      }
+        y[i].style.color = "blue";
+        y[i].style.fontFamily = "papyrus";
+        }
+
+  click++;
+}
+
+else if (click == 1) {
+
+  document.body.style.backgroundColor = "white";
+  var x = document.getElementsByClassName("greencolor");
+  var i;
+  for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#444";
+        x[i].style.fontFamily = "serif";
+        }
+  var y = document.getElementsByClassName("bluecolor");
+  for (i = 0; i < y.length; i++) {
+        y[i].style.color = "#444";
+        y[i].style.fontFamily = "serif";
+        }
+
+  click--;
+}
 }
