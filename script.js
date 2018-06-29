9a54afff660d6ca170b9491774275ae044b28bc0
AyarosF <sorayafructuoso@gmail.com>
2018-06-29 17:16:20 +0200

    Changement de couleur et police sans classes

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ function onClick() {
 if (click == 0) {
 
   document.body.style.background = "magenta";
-  var x = document.getElementsByClassName("greencolor");
+  var x = document.querySelectorAll("h1,h2,h3");
   var i;
   for (i = 0; i < x.length; i++) {
         x[i].style.color = "green";
         x[i].style.fontFamily = "Comic Sans Ms";
         }
-  var y = document.getElementsByClassName("bluecolor");
+  var y = document.getElementsByTagName("p");
   for (i = 0; i < y.length; i++) {
         y[i].style.color = "blue";
         y[i].style.fontFamily = "papyrus";
@@ -29,13 +29,13 @@ if (click == 0) {
 else if (click == 1) {
 
   document.body.style.backgroundColor = "white";
-  var x = document.getElementsByClassName("greencolor");
+  var x = document.querySelectorAll("h1,h2,h3");
   var i;
   for (i = 0; i < x.length; i++) {
         x[i].style.color = "#444";
         x[i].style.fontFamily = "serif";
         }
-  var y = document.getElementsByClassName("bluecolor");
+  var y = document.getElementsByTagName("p");
   for (i = 0; i < y.length; i++) {
         y[i].style.color = "#444";
         y[i].style.fontFamily = "serif";
